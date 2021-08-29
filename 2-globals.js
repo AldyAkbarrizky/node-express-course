d16d38d21913dbefb15a037db488602e40ea3caa
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2021-08-29 20:59:47 +0700

    First commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - TIDAK MEMILIKI OBJEK WINDOW !!!!
+
+// __dirname - Menampilkan path ke folder dimana aplikasi dijalankan
+// __filename - Menampilkan nama file yang sedang dieksekusi
+// require - Function yang digunakan untuk menggunakan suatu modul tertentu
+// module - Menampilkan info terkait modul yang sedang dieksekusi
+// process - Menampilkan info terkait environment dimana program sedang dieksekusi
+
+console.log(__dirname)
+console.log(__filename)
+setInterval(() => {
+    console.log('Hi!!!')
+}, 1000)
